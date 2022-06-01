62a3834a965658c18ce4bf85f6d23d298c78826c
Nate Traugh <n.traugh@yahoo.com>
2022-06-01 15:22:03 -0400

    added 'this' to the apiKey, added an argument for our fetchweather function, data for any city now displays in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,10 @@ var weather = {
     // logging our api key we got from openweather
     apiKey: "f3815a7ae5443a3f2cfb1fccc44d33f1",
     // creating a function to fetch the weather from the url 
-    fetchWeather: function(){
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=Charlotte&units=imperial&appid=f3815a7ae5443a3f2cfb1fccc44d33f1"
-        ).then((response) => response.json())
+    fetchWeather: function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey)
+        .then((response) => response.json())
         .then((data) => console.log(data));
     }
+    
 }
